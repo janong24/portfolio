12e4c3820d1a85ae2c464a5b5c64d068a4149b10
Jan Ong <janong24@gmail.com>
2023-01-02 14:04:34 -0500

    Dependency cleanup

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Header.scss'
 import CTA from './CTA'
+import Socials from './Socials'
 import profpic from '../../assets/profpic.png'
 
 function Header() {
@@ -11,9 +12,13 @@ function Header() {
         <h1>Jan Ong</h1>
         <h5 className="text-light">Front-end Developer</h5>
         <CTA />
+        <Socials />
+
         <div className="profilepic">
           <img src="{profpic}" alt="Profile Picture" />
         </div>
+
+        <a href='#contact' className='scroll__down'>Scroll Down</a>
       </div>
     </header>
 
--- src/components/header/Socials.jsx
+++ src/components/header/Socials.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import {SlSocialLinkedin} from 'react-icons/sl'
+import {SlSocialGithub} from 'react-icons/sl'
+import {SlSocialDribbble} from 'react-icons/sl'
 
 const Socials = () => {
   return (
     <div className='header__socials'>
-      <a href='https://linkedin.com' target='_blank'></a>
-      <a href='https://github.com' target='_blank'></a>
-      <a href='https://dribble.com' target='_blank'></a>
+      <a href='https://linkedin.com' target='_blank'><SlSocialLinkedin /></a>
+      <a href='https://github.com' target='_blank'><SlSocialGithub /></a>
+      <a href='https://dribbble.com' target='_blank'><SlSocialDribbble /></a>
     </div>
   )
 }
